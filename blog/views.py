757d9d19b57b05fef9fbbf75b4c1ad14b29a80da
kuma7777 <sakura.mp5.777@gmail.com>
2019-04-02 22:54:52 +0900

    chenge

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request): # 下のURLをテンプレート表示するRenderを返す
-    return render(request, 'blog/post_list.html', {}) 
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') # postsにクエリセットを代入
+    return render(request, 'blog/post_list.html', {'posts': posts}) # クエリセットを指定
