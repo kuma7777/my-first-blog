662c654253d2918938dd0c4666efacac902693dd
kuma7777 <sakura.mp5.777@gmail.com>
2019-04-03 18:32:12 +0900

    templatelink make

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'), # post_listをルートURLに割り当て
+    path('post/<int:pk>)/', views.post_detail, name='post_detail'), # pkに数字が入る
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
 def post_list(request): # 下のURLをテンプレート表示するRenderを返す
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') # postsにクエリセットを代入
     return render(request, 'blog/post_list.html', {'posts': posts}) # クエリセットを指定
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
